d0558d35530562679123c5c4896c35755e0cb2cc
Manbo- <manbo-@server.fake>
2013-05-09 00:14:57 +0900

    remove downloader

--- lib/bot2ch.rb
+++ lib/bot2ch.rb
@@ -1,6 +1,5 @@
 require 'open-uri'
 require 'kconv'
-require 'yaml/store'
 require 'net/http'
 
 require "bot2ch/version"
@@ -9,11 +8,6 @@ require "bot2ch/board"
 require "bot2ch/thread"
 require "bot2ch/post"
 require "bot2ch/thread"
-
-require "bot2ch/downloader"
-require "bot2ch/normal_image_downloader"
-require "bot2ch/imepita_downloader"
-require "bot2ch/app"
 
 module Bot2ch
   # Your code goes here...
