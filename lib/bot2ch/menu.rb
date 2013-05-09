a00b84db6ee25b731f6165940f769132501c769f
Manbo- <manbo-@server.fake>
2013-05-09 18:08:02 +0900

    fix menu

--- lib/bot2ch/menu.rb
+++ lib/bot2ch/menu.rb
@@ -2,23 +2,26 @@ module Bot2ch
   class Menu
     BBS_MENU = 'http://menu.2ch.net/bbsmenu.html'
 
-    def boards
-      boards = open(BBS_MENU).readlines.map do |line|
-        Board.new($1) if line =~ regexp
+    class << self
+      def boards
+        boards = open(BBS_MENU).readlines.map do |line|
+          Board.new($1) if line =~ regexp
+        end
+        boards.compact
       end
-      boards.compact
-    end
 
-    def get_board(subdir)
-      open(BBS_MENU).readlines.each do |line|
-        return Board.new($1) if line =~ regexp(subdir)
+      def get_board(subdir)
+        open(BBS_MENU).readlines.each do |line|
+          return Board.new($1) if line =~ regexp(subdir)
+        end
+        raise ArgumentError
       end
-    end
 
-    private
+      private
 
-    def regexp(subdir = nil)
-      subdir ? /href=(.+#{subdir})/i : /href=([^\s>"]+)/i
+      def regexp(subdir = nil)
+        subdir ? /href=(.+\/#{subdir}\/)/i : /href=([^\s>"]+)/i
+      end
     end
   end
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,19 +1,16 @@
 require "spec_helper"
 
 describe Bot2ch::Menu do
-  before{ @menu = Bot2ch::Menu.new }
-  let(:board){ "ff" }
-
   it do
     VCR.use_cassette("boards") do
-      @boards = @menu.boards
+      @boards = Bot2ch::Menu.boards
     end
     @boards.should be_a_kind_of(Array)
   end
 
   it do
     VCR.use_cassette("get_board") do
-      @get_board = @menu.get_board(board)
+      @get_board = Bot2ch::Menu.get_board("ff")
     end
     @get_board.should be_a_kind_of(Bot2ch::Board)
   end
