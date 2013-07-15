3ab5d364150a0030033a7016a3a5cd00ab115e13
Manbo- <manbo-@server.fake>
2013-07-15 17:34:11 +0900

    cache menu

--- lib/bot2ch/menu.rb
+++ lib/bot2ch/menu.rb
@@ -4,20 +4,28 @@ module Bot2ch
 
     class << self
       def boards
-        boards = open(BBS_MENU).readlines.map do |line|
+        boards = menu.readlines.map do |line|
           Board.new($1) if line =~ regexp
         end
         boards.compact
       end
 
       def get_board(subdir)
-        open(BBS_MENU).readlines.each do |line|
+        menu.readlines.each do |line|
           return Board.new($1) if line =~ regexp(subdir)
         end
         raise ArgumentError
       end
 
+      def reload
+        @menu = open(BBS_MENU)
+      end
+
       private
+
+      def menu
+        @menu ||= open(BBS_MENU)
+      end
 
       def regexp(subdir = nil)
         subdir ? /href=(.+\/#{subdir}\/)/i : /href=([^\s>"]+)/i
