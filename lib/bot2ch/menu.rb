c13bbf727228979fddd082fba32b9b6e8475d3db
Manbo- <manbo-@server.fake>
2013-07-15 18:13:35 +0900

    fix cache

--- lib/bot2ch/menu.rb
+++ lib/bot2ch/menu.rb
@@ -1,30 +1,31 @@
 module Bot2ch
   class Menu
     BBS_MENU = 'http://menu.2ch.net/bbsmenu.html'
+    @@menu = nil
 
     class << self
       def boards
-        boards = menu.readlines.map do |line|
+        boards = menu.map do |line|
           Board.new($1) if line =~ regexp
         end
         boards.compact
       end
 
       def get_board(subdir)
-        menu.readlines.each do |line|
+        menu.each do |line|
           return Board.new($1) if line =~ regexp(subdir)
         end
         raise ArgumentError
       end
 
       def reload
-        @menu = open(BBS_MENU)
+        @@menu = open(BBS_MENU).readlines
       end
 
       private
 
       def menu
-        @menu ||= open(BBS_MENU)
+        @@menu ? @@menu : reload
       end
 
       def regexp(subdir = nil)
