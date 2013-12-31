36cf3fc250f25a8c5ac6b8ea35d0c433dec0caa0
Manbo- <Manbo-@server.fake>
2013-12-31 15:42:34 +0900

    fix faraday-response-encoder

--- lib/bot2ch/client.rb
+++ lib/bot2ch/client.rb
@@ -17,10 +17,10 @@ module Bot2ch
     end
 
     def initialize
-      encoder_options = { source: "SJIS", encoding: "UTF-8", text_only: true, replace: "",
+      options = { from: "SJIS", to: "UTF-8", text_only: true, replace: "",
         if: ->(env){ env[:url].to_s =~ %r(\Ahttp://[^\/]+\.2ch\.net/) } }
       @client = Faraday.new do |builder|
-        builder.response :encoder, encoder_options
+        builder.response :encoder, options
         builder.use FaradayMiddleware::FollowRedirects, limit: 3
         builder.use Faraday::Response::RaiseError
         builder.adapter :net_http
